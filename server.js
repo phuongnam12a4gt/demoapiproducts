72214fe702e7adb5bf361782daa2fc1a5fe1d08b
phuongnam12a4gt <phuongnam12a4gt@gmail.com>
2021-03-20 22:55:22 -0700

    create api

---
+++ server.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser')
+let port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+let routes = require('./api/routes.js') //importing route
+routes(app)
+app.listen(port);
+
+console.log('RESTful API server started on: ' + port);
